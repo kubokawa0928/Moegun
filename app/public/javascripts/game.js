885fb1244800f6f8ad951d0e358a8f9f4d0a14f3
Naoki Kubokawa <nkubokawa@gmail.com>
2017-04-06 23:38:16 +0900

    とりあえずenchantでクマ出すところまで

--- app/public/javascripts/game.js
+++ app/public/javascripts/game.js
@@ -1,3 +1,29 @@
+enchant();
+
+window.onload = function(){
+    var game = new Core(320, 320);
+    game.fps = 15;
+    game.preload("images/chara1.png");
+    game.onload = function(){
+        var bear = new Sprite(32, 32);
+        bear.image = game.assets["images/chara1.png"];
+        bear.x = 0;
+        bear.y = 0;
+        bear.frame = 5;
+        game.rootScene.addChild(bear);
+
+        bear.addEventListener("enterframe", function(){
+            this.x += 1;
+            this.frame = this.age % 2 + 6;
+        });
+
+        bear.addEventListener("touchstart", function(){
+            game.rootScene.removeChild(bear);
+        });
+    };
+    game.start();
+};
+
 // 1.イベントとコールバックの定義
 var socketio = io.connect('http://localhost:3000');
 
